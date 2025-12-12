f44df99f00224547f94ee569888bdf6e15d16de9
nithishmaile <132052881+nithishmaile@users.noreply.github.com>
2025-12-12 17:23:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
+	let newMap=new Map()
+	for(let char of str){
+		newMap.set(char,(newMap.get(char) || 0)+1)
+	}
+
+	for (let [key,value] of newMap){
+		if(value==1){
+			return key
+		}
+	}
+	return null
  // Write your code here
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
